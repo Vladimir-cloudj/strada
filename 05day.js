ba09497496ece77d430e7bcf9745523f7f1cee50
Vladimir <wizart88@gmail.com>
2022-10-13 00:52:29 +0400

    feat: add  5 & 6 days

---
+++ 05day.js
@@ -0,0 +1,26 @@
+let a = 1;
+let b = 99;
+let c = a + b;
+
+let learn;
+learn = "text";
+
+let result;
+result = a == b;
+
+console.log(c);
+console.log(learn);
+console.log(result);
+console.log(typeof c);
+console.log(typeof learn);
+console.log(typeof result);
+// task#1
+let admin;
+let name;
+name = "Джон";
+admin = name;
+
+alert(admin);
+// task#2
+let ourPlanetName = 'Earth',
+		currentUserName = 'Denis';
